692183c20dbe94d28ae51d0a878041e511f46ce3
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-09 12:53:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here	const dat1=Date.UTC(
+	var dat1=Date.UTC(
 		parseInt(dat1.substring(0,4)),
 		parseInt(dat1.substring(5,7))-1,
 		parseInt(dat1.substring(8)),	
 	);
 
-	const date2=Date.UTC(
-		parseInt(date2.substring(0,4)),
-		parseInt(date2.substring(5,7))-1,
-		parseInt(date2.substring(8)),	
+	var dat2=Date.UTC(
+		parseInt(dat2.substring(0,4)),
+		parseInt(dat2.substring(5,7))-1,
+		parseInt(dat2.substring(8)),	
 	);
-	const timediff =date2-date1;
-	const days=Math.floor(timediff/24*60*60*60);
+	var timediff =dat2-dat1;
+	var days=Math.floor(timediff/(24*60*60*60));
 	return days;
-};
+}
+
 
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
+
 const dateTwo = prompt("Enter End Date.");
+
 alert(dateDiffInDays(dateOne, dateTwo));
+
 
