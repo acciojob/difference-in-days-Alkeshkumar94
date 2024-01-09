b47f020913ac45204e6851bba7b6f4e927f87d56
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-09 17:42:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
- function  d(date1, date2) {
+ var difference = function(date1, date2) {
 	var dat1=Date.UTC(
-		parseInt(dat1.substring(0,4)),
-		parseInt(dat1.substring(5,7))-1,
-		parseInt(dat1.substring(8)),	
+		parseInt(date1.substring(0,4)),
+		parseInt(date1.substring(5,7))-1,
+		parseInt(date1.substring(8)),	
 	);
 
 	var dat2=Date.UTC(
-		parseInt(dat2.substring(0,4)),
-		parseInt(dat2.substring(5,7))-1,
-		parseInt(dat2.substring(8)),	
+		parseInt(date2.substring(0,4)),
+		parseInt(date2.substring(5,7))-1,
+		parseInt(date2.substring(8)),	
 	);
-	var timediff =dat2-dat1;
+	var timediff =date2-date1;
 	var days=Math.floor(timediff/(24*60*60*60*1000));
-	return days;
+return days;
 }
 
 
@@ -21,7 +21,9 @@ const dateOne = prompt("Enter Start Date.");
 
 const dateTwo = prompt("Enter End Date.");
 
-alert(d(dateOne, dateTwo));
+alert(difference(dateOne, dateTwo));
+
+
 
 
 
